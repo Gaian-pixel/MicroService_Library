185a7a406e8bd45b972a581034342daba96cacb5
Gaian-pixel <giorgiogai62@gmail.com>
2021-11-12 22:19:02 +0100

    Starting setting up the book api

--- Books/src/main/java/model/Book.java
+++ Books/src/main/java/model/Book.java
@@ -1,4 +1,20 @@
 package model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Document
 public class Book {
+
+    @Id
+    private String bookId;
+    @NotBlank(message = "title is mandatory")
+    private String title;
+    private String author;
+    private String publisher;
+    private int pageNumber;
 }
