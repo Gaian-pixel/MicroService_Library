0cd7a55bdc47c27373459038ae4842bf5a3523df
Giorgio Gai <giorgiogai62@gmail.com>
2021-11-27 12:38:40 +0100

    Delete customer directory

--- customer/dbinit.sql
+++
@@ -1,9 +0,0 @@
-create database customer;
-use customer;
-create table customers(customerId varchar(20) not null,
-name varchar(10) not null,
-surname varchar(10) not null,
-address varchar(20),
-telNum varchar(10),
-primary key(customerId)
-);
