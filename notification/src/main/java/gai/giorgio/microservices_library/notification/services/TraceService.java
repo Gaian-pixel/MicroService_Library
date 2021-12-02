e0a3a692ea61a109d2122e559619ce8d343bc338
Giorgio Gai <giorgiogai62@gmail.com>
2021-12-02 19:43:36 +0100

    Added notification service

---
+++ notification/src/main/java/gai/giorgio/microservices_library/notification/services/TraceService.java
@@ -0,0 +1,23 @@
+package gai.giorgio.microservices_library.notification.services;
+
+
+import brave.Span;
+import brave.Tracer;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TraceService {
+
+    Tracer tracer;
+
+    public TraceService(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    public String getTraceId() {
+        Span span = tracer.currentSpan();
+        String traceId = span.context().traceIdString();
+        return traceId;
+    }
+}
