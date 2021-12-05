0bd1bce8dbbbf323b8a56da3ce75b4ed2671b205
Giorgio Gai <giorgiogai62@gmail.com>
2021-12-05 16:28:19 +0100

    Add files via upload

---
+++ borrowing & comunication/borrowing/src/main/java/gai/giorgio/microservices_library/borrowing/model/Borrowing.java
@@ -0,0 +1,22 @@
+package gai.giorgio.microservices_library.borrowing.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Document
+public class Borrowing {
+    @Id
+    private String borrowingId;
+    @NotBlank(message = "bookId is mandatory")
+    private String bookId;
+    @NotBlank(message = "customerId is mandatory")
+    private String customerId;
+    @NotBlank(message = "borrowingDate is mandatory")
+    private String borrowingDate;
+    @NotBlank(message = "borrowingExpDate is mandatory")
+    private String borrowingExpDate;
+}
