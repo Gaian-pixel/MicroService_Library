5158c47c6235d1fd70c46ca6360401079a2f497b
Giorgio Gai <giorgiogai62@gmail.com>
2021-12-05 17:18:38 +0100

    Added test class for borrowing service

--- borrowing & comunication/borrowing/src/main/java/gai/giorgio/microservices_library/borrowing/resolvers/GraphQlQuery.java
+++ borrowing & comunication/borrowing/src/main/java/gai/giorgio/microservices_library/borrowing/resolvers/GraphQlQuery.java
@@ -4,15 +4,19 @@ package gai.giorgio.microservices_library.borrowing.resolvers;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import gai.giorgio.microservices_library.borrowing.model.Borrowing;
 import gai.giorgio.microservices_library.borrowing.repos.BorrowingRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
+@RequestMapping(value = "/v2/borrowing/graphql")
 @Component
 public class GraphQlQuery implements GraphQLQueryResolver {
     public BorrowingRepository allBorrowing(Borrowing borrowing) {
