5077bb2a6a6c1fba2c9cbb63d2f33f75a8ab80c5
Giorgio Gai <giorgiogai62@gmail.com>
2021-12-05 16:26:58 +0100

    Delete borrowing directory

--- borrowing/src/main/java/gai/giorgio/microservices_library/borrowing/exceptions/BorrowingNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package gai.giorgio.microservices_library.borrowing.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Borrowing not found")
-public class BorrowingNotFoundException extends  RuntimeException{
-
-}
--- borrowing/src/main/java/gai/giorgio/microservices_library/borrowing/repos/BorrowingRepository.java
+++
@@ -1,8 +0,0 @@
-package gai.giorgio.microservices_library.borrowing.repos;
-
-import gai.giorgio.microservices_library.borrowing.model.Borrowing;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-public interface BorrowingRepository  extends MongoRepository<Borrowing, String> {
-}
