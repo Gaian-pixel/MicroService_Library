bae7803ad2cacf367bf791a26fee03cd0b3d1153
Giorgio Gai <giorgiogai62@gmail.com>
2021-12-10 19:44:58 +0100

    Add files via upload

--- borrowing & comunication/borrowing/src/main/java/gai/giorgio/microservices_library/borrowing/resolvers/GraphQlQuery.java
+++ borrowing & comunication/borrowing/src/main/java/gai/giorgio/microservices_library/borrowing/resolvers/GraphQlQuery.java
@@ -18,113 +18,17 @@ import java.util.Optional;
 @Slf4j
 @Component
 public class GraphQlQuery implements GraphQLQueryResolver {
-    public BorrowingRepository allBorrowing(Borrowing borrowing) {
-        BorrowingRepository borrowingRepository = new BorrowingRepository() {
-            @Override
+    public Borrowing allBorrowing() {
+        Borrowing borrowing = new Borrowing() {
+
             public <S extends Borrowing> List<S> saveAll(Iterable<S> iterable) {
                 return null;
             }
 
-            @Override
             public List<Borrowing> findAll() {
                 return null;
             }
-
-            @Override
-            public List<Borrowing> findAll(Sort sort) {
-                return null;
-            }
-
-            @Override
-            public <S extends Borrowing> S insert(S s) {
-                return null;
-            }
-
-            @Override
-            public <S extends Borrowing> List<S> insert(Iterable<S> iterable) {
-                return null;
-            }
-
-            @Override
-            public <S extends Borrowing> List<S> findAll(Example<S> example) {
-                return null;
-            }
-
-            @Override
-            public <S extends Borrowing> List<S> findAll(Example<S> example, Sort sort) {
-                return null;
-            }
-
-            @Override
-            public Page<Borrowing> findAll(Pageable pageable) {
-                return null;
-            }
-
-            @Override
-            public <S extends Borrowing> S save(S s) {
-                return null;
-            }
-
-            @Override
-            public Optional<Borrowing> findById(String s) {
-                return Optional.empty();
-            }
-
-            @Override
-            public boolean existsById(String s) {
-                return false;
-            }
-
-            @Override
-            public Iterable<Borrowing> findAllById(Iterable<String> iterable) {
-                return null;
-            }
-
-            @Override
-            public long count() {
-                return 0;
-            }
-
-            @Override
-            public void deleteById(String s) {
-
-            }
-
-            @Override
-            public void delete(Borrowing borrowing) {
-
-            }
-
-            @Override
-            public void deleteAll(Iterable<? extends Borrowing> iterable) {
-
-            }
-
-            @Override
-            public void deleteAll() {
-
-            }
-
-            @Override
-            public <S extends Borrowing> Optional<S> findOne(Example<S> example) {
-                return Optional.empty();
-            }
-
-            @Override
-            public <S extends Borrowing> Page<S> findAll(Example<S> example, Pageable pageable) {
-                return null;
-            }
-
-            @Override
-            public <S extends Borrowing> long count(Example<S> example) {
-                return 0;
-            }
-
-            @Override
-            public <S extends Borrowing> boolean exists(Example<S> example) {
-                return false;
-            }
         };
-        return borrowingRepository;
+        return borrowing;
     }
 }
