d1cd1d45b8e4f9ce7ced1d84e9dc963a50fc0acb
d.maggiorotto <d.maggiorotto@reply.it>
2022-03-25 12:48:09 +0100

    updated scripts

--- init_infrastructure_k8s.sh
+++ init_infrastructure_k8s.sh
@@ -1,73 +1,64 @@
 #!/bin/bash
 echo 'Microservice library infrastructure init on K8S started'  
 
+echo 'Namespcae creation started'
+
+kubectl create ns book
+kubectl create ns customer
+kubectl create ns borrowing-notification
+kubectl create ns logging-monitoring
+kubectl create ns vault
+
 echo 'Kafka infrastructure deploy started'
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
-( cd ./Kubernetes_Infrastracture_Components ; helm install --values kafka-values.yaml kafka bitnami/kafka )
+( cd ./Kubernetes_Infrastracture_Components ; helm install --values kafka-values.yaml kafka bitnami/kafka -n borrowing-notification )
 
-echo 'Kafka infrastructure deploy check --It may take a while--'
-
-kubectl get pods 
 
 echo 'Elasticsearch deploy started'
 
 helm repo add elastic https://helm.elastic.co
 
-( cd ./Kubernetes_Infrastracture_Components ; helm install --values elasticsearch-values.yaml elasticsearch elastic/elasticsearch )
+( cd ./Kubernetes_Infrastracture_Components ; helm install --values elasticsearch-values.yaml elasticsearch elastic/elasticsearch -n logging-monitoring )
 
-echo 'Elasticsearch deploy check --It may take a while--'
-
-kubectl get pods --namespace=default -l app=elasticsearch-master 
 
 echo 'Logstash deploy started'
 
-( cd ./Kubernetes_Infrastracture_Components ; helm install --values logstash-values.yaml logstash elastic/logstash )
+( cd ./Kubernetes_Infrastracture_Components ; helm install --values logstash-values.yaml logstash elastic/logstash -n logging-monitoring )
 
-echo 'Logstash deploy check --It may take a while--'
-
-kubectl get pods --namespace=default -l app=logstash-logstash
 
 echo 'Filebeat as Deamonset deploy started'
 
-( cd ./Kubernetes_Infrastracture_Components ; helm install --values filebeat-values.yaml filebeat elastic/filebeat )
+( cd ./Kubernetes_Infrastracture_Components ; helm install --values filebeat-values.yaml filebeat elastic/filebeat -n logging-monitoring )
 
-echo 'Filebeat deploy check --It may take a while--'
-
-kubectl get pods --namespace=default -l app=filebeat
 
 echo 'Kibana deploy started'
 
-( cd ./Kubernetes_Infrastracture_Components ; helm install --values kibana-values.yaml kibana elastic/kibana )
+( cd ./Kubernetes_Infrastracture_Components ; helm install --values kibana-values.yaml kibana elastic/kibana -n logging-monitoring)
 
-echo 'Kibana deploy check --It may take a while--'
-
-kubectl get pods --namespace=default -l app=kibana
 
 echo 'MongoDb for books service deploy started'
 
-( cd ./Kubernetes_Db_Creation ; helm install --values mongodb-book-values.yaml book-mongodb bitnami/mongodb )
+( cd ./Kubernetes_Db_Creation ; helm install --values mongodb-book-values.yaml book-mongodb bitnami/mongodb -n book)
 
-echo 'MongoDB for books service deploy check --It may take a while--'
-
-kubectl get pods | grep mongo
 
 echo 'MongoDb for borrowing service deploy started'
 
-( cd ./Kubernetes_Db_Creation ; helm install --values mongodb-borrowing-values.yaml borrowing-mongodb bitnami/mongodb )
-
-echo 'MongoDB for borrowing service deploy check --It may take a while--'
-
-kubectl get pods | grep mongo
+( cd ./Kubernetes_Db_Creation ; helm install --values mongodb-borrowing-values.yaml borrowing-mongodb bitnami/mongodb -n borrowing-notification )
 
 
 echo 'Postgres for customer service deploy started'
 
-( cd ./Kubernetes_Db_Creation ; helm install --values postgres-customer-values.yaml customer-postgres bitnami/postgresql )
+( cd ./Kubernetes_Db_Creation ; helm install --values postgres-customer-values.yaml customer-postgres bitnami/postgresql -n customer)
 
-echo 'Postgres for customer service deploy check --It may take a while--'
 
-kubectl get pods | grep postgres
+echo 'Vault deploy started'
+
+helm repo add hashicorp https://helm.releases.hashicorp.com
+
+helm repo update
+
+helm install vault hashicorp/vault --set "server.dev.enabled=true" -n vault
 
 echo 'Deploy terminated...'
