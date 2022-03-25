2fdd25c1de7ac50dd05caf11a216f4fd76a89afb
Gaian-pixel <d.maggiorotto@reply.it>
2022-03-25 17:14:04 +0100

    Fixed deployment

--- init_services_k8s.sh
+++ init_services_k8s.sh
@@ -2,7 +2,7 @@
 echo 'Microservice library init on K8S started'  
 
 
-( cd ./book ; kubectl apply -f book-ms.yaml -n book )
-( cd ./customer ; kubectl apply -f customer-ms.yaml -n customer )
-( cd ./borrowing-notification/borrowing ; kubectl apply -f borrowing-ms.yaml -n borrowing-notification )
-( cd ./borrowing-notification/notification ; kubectl apply -f notification-ms.yaml -n borrowing-notification )
+( cd ./book ; kubectl apply -f book-ms.yaml )
+( cd ./customer ; kubectl apply -f customer-ms.yaml )
+( cd ./borrowing-notification/borrowing ; kubectl apply -f borrowing-ms.yaml )
+( cd ./borrowing-notification/notification ; kubectl apply -f notification-ms.yaml )
